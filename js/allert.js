a6f88935d6d5e083d809ac2a429d35a20de74567
VadimTrubay <vadnetvadnet@ukr.net>
2023-01-10 13:53:09 +0200

    main

---
+++ js/allert.js
@@ -0,0 +1,13 @@
+'use strict';
+// Этот комментарий занимает всю строку
+// alert('Hello');
+// [1, 2].forEach(alert);
+/* Пример с двумя сообщениями.
+Это - многострочный комментарий.
+*/
+let user = 'Vad';
+let age = 25;
+let mess = 'hello vAD';
+alert(mess)
+alert(age)
+alert(user)
---
+++ js/temp.js
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html>
+<body>
+  <p>Перед скриптом...</p>
+  <script>
+    alert( 'Привет, мир!' );
+  </script>
+  <p>...После скрипта.</p>
+</body>
+</html>
